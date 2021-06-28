e2dafb26f1f5395cbd8835af5da7d34b7d07e483
Alonso Parra <alonsogparra@gmail.com>
2021-06-28 11:18:22 -0400

    App built as base component of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer, useState } from "react";
+import "../node_modules/animate.css";
+import { AddTask } from "./components/AddTask";
+import { ListTasks } from "./components/ListTasks";
+import "./components/sytles.css";
+import { reducer } from "./store/reducer/reducer";
+import { types } from "./store/types/types";
 
-function App() {
+const init = () => {
+  return JSON.parse(localStorage.getItem("tasks")) || [];
+};
+
+export const App = () => {
+  const [tasks, dispatch] = useReducer(reducer, [], init);
+  const [showComplete, setShowComplete] = useState(false);
+
+  const handleAddTask = (newTask) => {
+    dispatch({
+      type: types.add,
+      payload: newTask,
+    });
+  };
+
+  const handleToggleTask = (id) => {
+    dispatch({
+      type: types.toggle,
+      payload: id,
+    });
+  };
+
+  const handleDeleteTask = (id) => {
+    console.log("App handleDeleteTask", id);
+    dispatch({
+      type: types.delete,
+      payload: id,
+    });
+  };
+
+  const handleEditTask = (id, newDesc) => {
+    dispatch({
+      type: types.update,
+      payload: {
+        id,
+        desc: newDesc,
+      },
+    });
+  };
+
+  const handleSearchTask = (desc) => {
+    dispatch({
+      type: types.search,
+      payload: desc,
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container-fluid animate__animated animate__fadeIn">
+        <h1>Task Manager (Tasks Quantity: {tasks.length})</h1>
+        <h3>Tasks Done: {tasks.filter((task) => task.done).length}</h3>
+        <hr />
+        <div className="row mb-2">
+          <div className="">
+            <input
+              type="checkbox"
+              value={showComplete}
+              name="showComplete"
+              id="showComplete"
+              className="me-1"
+              onChange={() => setShowComplete(!showComplete)}
+            />
+            <label htmlFor="">Show Tasks Completed</label>
+          </div>
+        </div>
 
-export default App;
+        <div className="row">
+          <div className="col-12 col-md-5 mb-4">
+            <AddTask handleAddTask={handleAddTask} />
+          </div>
+          <div className="col-12 col-md-7">
+            <ListTasks
+              tasks={tasks}
+              handleToggleTask={handleToggleTask}
+              handleDeleteTask={handleDeleteTask}
+              handleEditTask={handleEditTask}
+              showComplete={showComplete}
+              handleSearchTask={handleSearchTask}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
